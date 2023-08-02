a1a976513f8a6dfbff5376a721c4365bdd9d84f4
masumahemd <rimasum01724@gmail.com>
2023-08-02 21:08:42 +0600

    upload code

--- module18/parameter.js
+++ module18/parameter.js
@@ -11,8 +11,14 @@ function sum(a,b,c,d,e,f){
       console.log(a,b,c,d,e,f);
       var sum= a+b+c+d+e+f+a+b+c;
       console.log(sum);
-
 }
 // calling sum function
 // sum(1,2,3,4,5,6);
 
+function library(money){
+    var printCopy=10;
+    var quantity=money/10;
+    return quantity;
+}
+var paper=library(500);
+ console.log(paper);
