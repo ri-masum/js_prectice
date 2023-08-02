d9ff12258790c2788d1bfd60f1a2705426b4368c
masumahemd <rimasum01724@gmail.com>
2023-08-02 10:36:32 +0600

    psuhing code

---
+++ module18/parameter.js
@@ -0,0 +1,18 @@
+function time(somoy){
+    console.log('the time is 12:20 PM');
+    console.log(somoy);
+
+}
+
+// var koyta="12:20"
+// time(koyta);
+
+function sum(a,b,c,d,e,f){
+      console.log(a,b,c,d,e,f);
+      var sum= a+b+c+d+e+f+a+b+c;
+      console.log(sum);
+
+}
+// calling sum function
+// sum(1,2,3,4,5,6);
+
