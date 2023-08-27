b536ea90c085b3f8e6c64a16dc28d3c570c7efd9
masumahemd <rimasum01724@gmail.com>
2023-08-27 23:59:50 +0600

    api

--- MileStone6/module33/post.js
+++ MileStone6/module33/post.js
@@ -1,24 +1,58 @@
-function loadData(){
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res=>res.json())
-    .then(data=>showData(data))
+function loadData() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => showData(data));
 }
 
-function showData(posts){
-
-    const output=document.getElementById('post-container');
-    for(const post of posts ){
-
-    const divContainer=document.createElement('div')        
-    divContainer.innerHTML=`
+function showData(posts) {
+  const output = document.getElementById("post-container");
+  for (const post of posts) {
+    const divContainer = document.createElement("div");
+    divContainer.classList.add("post");
+    divContainer.innerHTML = `
     <h4>User- ${post.userId}</h4>
     <h5>Title:${post.title}</h5>
     <p>Description:${post.body}</p>
-    `
-    output.appendChild(divContainer)
-
-    }
-
+    `;
+    output.appendChild(divContainer);
+  }
 }
 
-loadData()
+loadData();
+
+function deleteApost() {
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "DELETE",
+  });
+}
+
+function updateApost() {
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "PUT",
+    body: JSON.stringify({
+      id: 1,
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
+
+function patchApost() {
+  fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "PATCH",
+    body: JSON.stringify({
+      title: "foo",
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+}
---
+++ MileStone6/module33/test.js
@@ -0,0 +1,5 @@
+
+
+
+const stuInfo = JSON.stringify({ name: "James", roll: 3 }); 
+console.log(stuInfo.name);
