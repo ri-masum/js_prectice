92236194d39743b3e95078172d7127e375b53af9
masumahemd <rimasum01724@gmail.com>
2023-08-28 17:04:09 +0600

    module 34

--- MileStone6/prectice/task3.js
+++ MileStone6/prectice/task3.js
@@ -8,6 +8,40 @@ const instructor=[
 // console.log(seniorPosition);
 const output=instructor.filter(ins=>ins.position==='senior');
 for(let i in output){
-    console.log('senior developer :',output[i].name);
+    // console.log('senior developer :',output[i].name);
 
 }
+
+const dreamGirl = [
+    {
+     sokina: {
+      name: "bbu",
+      height: "5.4",
+      family: [{ father: "rock", mother: "shila", sister: "chinki" }],
+      age: undefined,
+      contactInfo: [
+       {
+        facebook: {
+         link: "https://www.facebook.com/",
+         followers: "12545",
+         status: "single",
+         friendsList: [{ name: "rofik" }, undefined],
+        },
+       },
+       { instagram: "https://www.instagram.com/" },
+      ],
+     },
+    },
+   ];
+
+//    console.log(dreamGirl[0].sokina.contactInfo[1].instagram);
+
+
+const phones = [
+ { name: "sony", price: 500 },
+ { name: "apple", price: 700 },
+ { name: "sony", price: 700 },
+];
+
+const result=phones.filter((phones)=>phones.price !=500)
+console.log(result);
